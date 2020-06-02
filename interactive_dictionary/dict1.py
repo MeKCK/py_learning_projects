c9b54a66ba2c3894598eeb0476077f2f1fe235a1
kiprono <42340517+MeKCK@users.noreply.github.com>
2020-06-02 21:35:01 +0300

    user_friendly

--- interactive_dictionary/dict1.py
+++ interactive_dictionary/dict1.py
@@ -8,16 +8,22 @@ def translate(w):
     if w in data:
         return data[w]
     elif len(get_close_matches(w, data.keys())) > 0:
-        yn = input("Did you mean %s instead? Enter Y if yes, or N if no:" % get_close_matches(w, data.keys())[0]
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
         if yn == "Y":
             return data[get_close_matches(w, data.keys())[0]]
         elif yn == "N":
             return "the word doesn't exist. check it!!!"
-            else:
-                return "We didnt understand your input."
+        else:
+            return "We didnt understand your input."
     else :
         return "The word doesn't exist. Please check it"
 
 word = input("Enter word: ")
 
-print(translate(word))
+output = translate(word)
+
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
