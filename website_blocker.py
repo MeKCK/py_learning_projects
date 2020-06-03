d7480bc24cca8ca44d46bf35faad3f654e0c6c04
kiprono <42340517+MeKCK@users.noreply.github.com>
2020-06-03 10:44:43 +0300

    time factor

--- website_blocker.py
+++ website_blocker.py
@@ -7,7 +7,7 @@ redirect="127.0.0.1"
 website_list=["www.facebook.com","facebook.com", "www.pintrest.com","pintrest.com"]
 
 while True:
-    if dt(dt.now().year,dt.now().month,dt.now().day,8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,16):
+    if dt(dt.now().year,dt.now().month,dt.now().day,16) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,16):
         print("Working Hours.....")
         with open(hosts_temp,'r+')as file:
             content=file.read()
@@ -17,5 +17,12 @@ while True:
                 else: 
                     file.write(redirect+" "+website+"\n")
     else:
+        with open(hosts_temp,'r+') as file:
+            content=file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            file.truncate()
         print("Fun hours.....")
     time.sleep(5)
