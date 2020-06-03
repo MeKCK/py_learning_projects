3ff7d45258fd05822811e405c5a46acbe61124b6
kiprono <42340517+MeKCK@users.noreply.github.com>
2020-06-03 06:49:08 +0300

    geo color code

---
+++ webmaps/README.md
@@ -0,0 +1,14 @@
+# About 
+Creating webmaps with markers to data driven locations 
+
+ ## Concepts Covered 
+-  loading and interacting with data from an external json file 
+- using folium library
+- 
+- 
+ 
+
+
+
+### License
+[MIT](https://choosealicense.com/licenses/mit/)
--- webmaps/map1.py
+++ webmaps/map1.py
@@ -1,7 +1,7 @@
-import folium 
-import pandas 
+import folium
+import pandas
 
-data= pandas.read_csv("Volcanoes_USA.txt")
+data = pandas.read_csv("Volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
 elev = list(data["ELEV"])
@@ -9,28 +9,26 @@ elev = list(data["ELEV"])
 def color_producer(elevation):
     if elevation < 1000:
         return 'green'
-    elif 1000<= elevation < 3000:
+    elif 1000 <= elevation < 3000:
         return 'orange'
     else:
         return 'red'
-
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Mapbox Bright")
 
-fgv = folium.FeatureGroup(name="My Map")
-
+fgv = folium.FeatureGroup(name="Volcanoes")
 
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.CircleMarker(location=[lt, ln], radius = 6, popup=str(el)+" m", 
-    fill_color=color_producer(el), color='grey', fill_opacity=0.7))
-
-fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig'), 
-style_function=lambda x: {'fillColor':'yellow' if x['properties']['POP2005'] < 10000000 else 'orange' if between 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+    fgv.add_child(folium.CircleMarker(location=[lt, ln], radius = 6, popup=str(el)+" m",
+    fill_color=color_producer(el), fill=True,  color = 'grey', fill_opacity=0.7))
 
 fgp = folium.FeatureGroup(name="Population")
 
+fgp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000
+else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+
 map.add_child(fgv)
 map.add_child(fgp)
-
-map.add_child(folium,LayerControl())
+map.add_child(folium.LayerControl())
 
 map.save("map1.html")
