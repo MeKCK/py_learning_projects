feaabf5966e162b6b3f3b3717ce460e21d003a53
kiprono <42340517+MeKCK@users.noreply.github.com>
2020-06-03 05:29:18 +0300

    markers

--- webmaps/map1.py
+++ webmaps/map1.py
@@ -1,8 +1,17 @@
 import folium 
+import pandas 
+
+data= pandas.read_csv("Volcanoes_USA.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Mapbox Bright")
 
 fg = folium.FeatureGroup(name="My Map")
-fg.add_child(folium.Marker(location=[38.2, -99.1], popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+
+for lt, ln in zip(lat, lon):
+    fg.add_child(folium.Marker(location=[lt, ln], popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+
 map.add_child(fg)
 
 map.save("map1.html")
