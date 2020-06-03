696328972b0290f9791adc811c4947ddb3256065
kiprono <42340517+MeKCK@users.noreply.github.com>
2020-06-03 06:31:06 +0300

    feature groups

--- webmaps/map1.py
+++ webmaps/map1.py
@@ -16,7 +16,8 @@ def color_producer(elevation):
 
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Mapbox Bright")
 
-fg = folium.FeatureGroup(name="My Map")
+fgv = folium.FeatureGroup(name="My Map")
+
 
 for lt, ln, el in zip(lat, lon, elev):
     fg.add_child(folium.CircleMarker(location=[lt, ln], radius = 6, popup=str(el)+" m", 
@@ -25,6 +26,11 @@ for lt, ln, el in zip(lat, lon, elev):
 fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig'), 
 style_function=lambda x: {'fillColor':'yellow' if x['properties']['POP2005'] < 10000000 else 'orange' if between 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
-map.add_child(fg)
+fgp = folium.FeatureGroup(name="Population")
+
+map.add_child(fgv)
+map.add_child(fgp)
+
+map.add_child(folium,LayerControl())
 
 map.save("map1.html")
