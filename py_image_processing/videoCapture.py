9586235685a74f3c7ba02e04d4fc6ba2fbf8ddd7
kiprono <42340517+MeKCK@users.noreply.github.com>
2020-06-04 22:50:30 +0300

    fps

--- py_image_processing/videoCapture.py
+++ py_image_processing/videoCapture.py
@@ -2,17 +2,25 @@ import cv2
 
 video=cv2.VideoCapture(0)
 
+
+
 video.release()
+a=1
+while True:
+    a=a+1
+    check, frame = video.read()
 
-check, frame = video.read()
+    print(check)
+    print(frame)
 
-print(check)
-print(frame)
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+   # time.sleep(3)
+    cv2.imshow("Capturing", gray)
 
-gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-time.sleep(3)
-cv2.imshow("Capturing", gray)
+    key=cv2.waitKey(1)
 
-cv.waitKey(0)
+    if key==ord('q'):
+        break
+
 video.release()
 cv2.destroyAllWindows
